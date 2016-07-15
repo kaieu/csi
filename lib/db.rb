ce47ed36e6d22f80ec853d2d584c1c755114fd47
Tomohiro Nakagawa <tomoemon@gmail.com>
2016-07-15 11:02:44 +0900

    Implement PostgreSQL handling.

--- lib/db.rb
+++ lib/db.rb
@@ -103,6 +103,78 @@ module Csi
 
     class PgConnection < Connection
       add_factory %w(postgres postgresql), "PostgreSQL Connector"
+
+      def initialize
+        begin
+          require 'pg'
+        rescue LoadError
+          raise "Required pg library is not found. Try 'gem install pg'."
+        end
+      end
+
+      def connect(connstr, user, pass)
+        host, port, dbname = parse_connstr(connstr)
+        params = {
+          host: host,
+          user: user,
+          password: pass
+        }
+        params[:port] = port if port
+        params[:dbname] = dbname if dbname
+
+        @pg = PGconn.open(params)
+      end
+
+      def parse_connstr(connstr)
+        connparts = connstr.split('/')
+        raise "illegal dburl(too many '/'): #{connstr}" if connparts.size > 2
+        hostparts = connparts[0].split(':')
+        raise "illegal dburl(too many ':'): #{connstr}" if hostparts.size > 2
+        return hostparts[0], hostparts[1], connparts[1]
+      end
+
+      def exec(sql)
+        @pg.exec(sql)
+      end
+
+      def query(sql)
+        res = @pg.exec(sql)
+        if res.is_a?(PGresult)
+          PgResult.new res
+        else
+          res
+        end
+      end
+
+      def close
+        @pg.finish if @pg
+        @pg = nil
+      end
+    end
+
+    class PgResult
+      def initialize(res)
+        @result = res
+      end
+
+      def columns
+        @result.fields
+      end
+
+      def each
+        return to_enum unless block_given?
+        @result.each do |r|
+          yield convert(r.map{|t| t[1]})
+        end
+      end
+
+      def convert(org)
+        org
+      end
+
+      def size
+        @result.ntuples
+      end
     end
   end
 end
