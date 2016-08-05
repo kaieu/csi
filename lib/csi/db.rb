eb63321bcf6c88354db8318ad432c5dcd1c23303
Tomohiro Nakagawa <tomoemon@gmail.com>
2016-08-05 07:02:26 -0700

    Organize lib dir

---
+++ lib/csi/db.rb
@@ -0,0 +1,6 @@
+# -*- conding: utf-8 -*-
+
+require 'csi/db/oracle'
+require 'csi/db/postgresql'
+
+# vim:set ts=2 sw=2 et:
--- lib/db.rb
+++
@@ -1,6 +0,0 @@
-# -*- conding: utf-8 -*-
-
-require 'db/oracle'
-require 'db/postgresql'
-
-# vim:set ts=2 sw=2 et:
